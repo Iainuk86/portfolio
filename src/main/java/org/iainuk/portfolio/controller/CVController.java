67f066ffd9ad3ece1f4b33153758ed9d35618edc
Iainuk86 <iainuk1986@gmail.com>
2021-03-30 17:27:49 +1100

    Added view CV option

--- src/main/java/org/iainuk/portfolio/controller/CVController.java
+++ src/main/java/org/iainuk/portfolio/controller/CVController.java
@@ -1,21 +1,40 @@
 package org.iainuk.portfolio.controller;
 
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 @Controller
 public class CVController {
 
+    @RequestMapping(value = "/viewcv")
+    public ResponseEntity<InputStreamResource> getTermsConditions() throws FileNotFoundException {
+
+        String filePath = "C:\\Users\\iainu\\IdeaProjects\\Portfolio\\src\\main\\resources\\";
+        String fileName = "iain-mcintosh-cv.pdf";
+        File file = new File(filePath + fileName);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("content-disposition", "inline;filename=" + fileName);
+
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(file.length())
+                .contentType(MediaType.parseMediaType("application/pdf"))
+                .body(resource);
+    }
+
     @RequestMapping(value = "/downloadcv", method = RequestMethod.GET)
-    public StreamingResponseBody getSteamingFile(HttpServletResponse response) throws IOException {
+    public StreamingResponseBody downloadCV(HttpServletResponse response) throws IOException {
 
         response.setContentType("text/html;charset=UTF-8");
         response.setHeader("Content-Disposition", "attachment; filename=\"iain-mcintosh-cv.pdf\"");
